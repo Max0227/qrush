37aec64e2ce69b0a5353f00284d821bc13d749a8
Max0227 <3dladesign@gmail.com>
2026-04-16 00:12:44 +0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// index.js — Заглушка для Vercel (перенаправляет на статику)
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('QRush');
-}
